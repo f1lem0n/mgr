2ae7d9af92a45b98b71370b0a3668ace57054e91
Filip Hajdyła <hajdylaf@gmail.com>
2024-04-12 13:25:49 +0200

    improve xml2fasta

--- blast_xml2fasta.py
+++ blast_xml2fasta.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 
 
 def find_query(tree):
-    qid = tree.find(".//Iteration_query-def").text
+    qid = tree.find(".//BlastOutput_query-def").text
     qseq = tree.find(".//Hsp_qseq").text
     return qid, qseq
 
@@ -11,23 +11,32 @@ def find_query(tree):
 def find_hits(tree):
     hids = tree.findall(".//Hit_def")
     hseqs = tree.findall(".//Hsp_hseq")
-    return [hid.text for hid in hids], [hseq.text for hseq in hseqs]
+    return (
+        [hid.text for hid in hids],
+        [hseq.text.replace("-", "") for hseq in hseqs],
+    )
 
 
-def write_fasta(ids, seqs):
-    with open("output/blastn/blastn.fasta", "w") as f:
+def write_fasta(ids, seqs, outfile):
+    with open(f"output/blastn/{outfile}.fasta", "w") as f:
         for i, s in zip(ids, seqs):
             f.write(f">{i}\n{s}\n")
 
 
 def main():
     for path in pathlib.Path("output/blastn").iterdir():
-        tree = ET.parse(path)
-        qid, qseq = find_query(tree)
-        hids, hseqs = find_hits(tree)
-        ids = [qid] + hids
-        seqs = [qseq] + hseqs
-        write_fasta(ids, seqs)
+        if path.suffix != ".xml":
+            continue
+        try:
+            tree = ET.parse(path)
+            qid, qseq = find_query(tree)
+            hids, hseqs = find_hits(tree)
+            ids = [qid] + hids
+            seqs = [qseq] + hseqs
+            write_fasta(ids, seqs, outfile=path.stem)
+        except AttributeError:
+            print(f"[{path.stem}] No hits found.")
+            continue
 
 
 if __name__ == "__main__":
