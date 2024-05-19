db18c9b5c7e8270b06b97ae94b81c8f4d2fc4145
Filip Hajdyła <hajdylaf@gmail.com>
2024-05-19 17:37:33 +0200

    update structure generation

--- scripts/RNAsmc.R
+++ scripts/RNAsmc.R
@@ -5,108 +5,177 @@ library(reshape2)
 library(tidyverse)
 library(dplyr)
 
-genes <- list.dirs("output", full.names = FALSE, recursive = FALSE)
-genes
+types <- list.dirs("output/structures/", full.names = FALSE, recursive = FALSE)
+programs <- c("vienna")
 
 invivo_vs_insilico <- c()
 invivo_vs_consensus <- c()
+invivo_vs_guided <- c()
 
-for (gene in genes) {
-  insilico <- loadCt(paste0("output/", gene, "/CT/insilico.ct"))
-  invivo <- loadCt(paste0("output/", gene, "/CT/invivo.ct"))
-  consensus <- loadCt(paste0("output/", gene, "/CT/consensus.ct"))
-  substructures <- list(
-    insilico = getSubStr(insilico),
-    invivo = getSubStr(invivo),
-    consensus = getSubStr(consensus)
-  )
-  score_matrix <- getCompare(substructures)
-  invivo_vs_insilico <- append(invivo_vs_insilico, score_matrix[2, 1])
-  invivo_vs_consensus <- append(invivo_vs_consensus, score_matrix[2, 3])
-  labs <- c("in silico", "in vivo", "consensus")
-  colnames(score_matrix) <- labs
-  rownames(score_matrix) <- labs
-  score_df <- melt(score_matrix)
-  colnames(score_df) <- c("x", "y", "value")
-  plot <- score_df %>%
-    ggplot(aes(x = x, y = y, fill = value)) +
-    geom_tile(
-      color = "black",
-      lwd = 0.5, linetype = 1
-    ) +
-    scale_fill_gradient(
-      low = "white", high = "red",
-      limits = c(0, 10)
-    ) +
-    geom_text(
-      aes(label = round(value, digits = 2)),
-      color = "black", size = 4
-    ) +
-    guides(
-      fill = guide_colourbar(
-        barwidth = 1,
-        barheight = 20,
-        frame.colour = "black",
-        ticks.colour = "black",
-        title = "Similarity score"
-      )
-    ) +
-    coord_fixed() +
-    ggtitle(paste0("RNA structure similarity of ", gene)) +
-    theme_minimal() +
-    theme(
-      title = element_text(size = 16),
-      axis.title = element_blank(),
-      axis.ticks = element_blank(),
-      panel.grid = element_blank(),
-      axis.text = element_text(size = 14),
-      plot.background = element_rect(fill = "white")
+for (program in programs) {
+  for (type in types) {
+    genes <- list.dirs(
+      paste0("output/structures/", type),
+      full.names = FALSE, recursive = FALSE
     )
-  ggsave(
-    paste0("output/", gene, "/similarity_heatmap.png"),
-    plot = plot,
-    width = 10, height = 10, dpi = 300
-  )
+    for (gene in genes) {
+      insilico <- loadCt(
+        paste0(
+          "output/structures/", type, "/",
+          gene, "/", program, "/CT/insilico.ct"
+        )
+      )
+      invivo <- loadCt(
+        paste0(
+          "output/structures/", type, "/",
+          gene, "/", program, "/CT/invivo.ct"
+        )
+      )
+      consensus <- loadCt(
+        paste0(
+          "output/structures/", type, "/",
+          gene, "/", program, "/CT/consensus.ct"
+        )
+      )
+      if (type == "mRNA") {
+        consensus_guided <- loadCt(
+          paste0(
+            "output/structures/", type, "/",
+            gene, "/", program, "/CT/consensus_guided.ct"
+          )
+        )
+        substructures <- list(
+          insilico = getSubStr(insilico),
+          invivo = getSubStr(invivo),
+          consensus = getSubStr(consensus),
+          consensus_guided = getSubStr(consensus_guided)
+        )
+        score_matrix <- getCompare(substructures)
+        invivo_vs_insilico <- append(invivo_vs_insilico, score_matrix[2, 1])
+        invivo_vs_consensus <- append(invivo_vs_consensus, score_matrix[2, 3])
+        invivo_vs_guided <- append(invivo_vs_guided, score_matrix[2, 4])
+        labs <- c("in silico", "in vivo", "consensus", "guided")
+        colnames(score_matrix) <- labs
+        rownames(score_matrix) <- labs
+        score_df <- melt(score_matrix)
+        colnames(score_df) <- c("x", "y", "value")
+      } else {
+        substructures <- list(
+          insilico = getSubStr(insilico),
+          invivo = getSubStr(invivo),
+          consensus = getSubStr(consensus)
+        )
+        score_matrix <- getCompare(substructures)
+        invivo_vs_insilico <- append(invivo_vs_insilico, score_matrix[2, 1])
+        invivo_vs_consensus <- append(invivo_vs_consensus, score_matrix[2, 3])
+        labs <- c("in silico", "in vivo", "consensus")
+        colnames(score_matrix) <- labs
+        rownames(score_matrix) <- labs
+        score_df <- melt(score_matrix)
+        colnames(score_df) <- c("x", "y", "value")
+      }
+      plot <- score_df %>%
+        ggplot(aes(x = x, y = y, fill = value)) +
+        geom_tile(
+          color = "black",
+          lwd = 0.5, linetype = 1
+        ) +
+        scale_fill_gradient(
+          low = "white", high = "red",
+          limits = c(0, 10)
+        ) +
+        geom_text(
+          aes(label = round(value, digits = 2)),
+          color = "black", size = 4
+        ) +
+        guides(
+          fill = guide_colourbar(
+            barwidth = 1,
+            barheight = 20,
+            frame.colour = "black",
+            ticks.colour = "black",
+            title = "Similarity score"
+          )
+        ) +
+        coord_fixed() +
+        ggtitle(paste0("RNA structure similarity of ", gene)) +
+        theme_minimal() +
+        theme(
+          title = element_text(size = 16),
+          axis.title = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid = element_blank(),
+          axis.text = element_text(size = 14),
+          plot.background = element_rect(fill = "white")
+        )
+      ggsave(
+        paste0(
+          "output/structures/", type, "/",
+          gene, "/", program, "/similarity_heatmap.png"
+        ),
+        plot = plot,
+        width = 10, height = 10, dpi = 300
+      )
+    }
+    if (type == "mRNA") {
+      df <- data.frame(
+        gene = genes,
+        VS = invivo_vs_insilico,
+        VC = invivo_vs_consensus,
+        VG = invivo_vs_guided
+      )
+    } else {
+      df <- data.frame(
+        gene = genes,
+        VS = invivo_vs_insilico,
+        VC = invivo_vs_consensus
+      )
+    }
+
+    write.table(
+      df,
+      paste0(
+        "output/structures/", type,
+        "/similarity_scores_", program, ".tsv"
+      ),
+      sep = "\t",
+      row.names = FALSE, quote = FALSE
+    )
+
+    df <- read.table(
+      paste0(
+        "output/structures/", type,
+        "/similarity_scores_", program, ".tsv"
+      ),
+      header = TRUE, sep = "\t"
+    )
+    df <- melt(df)
+    colnames(df) <- c("gene", "type", "value")
+    replacements <- c("VS" = "in silico", "VC" = "consensus", "VG" = "guided")
+    df$type <- str_replace_all(df$type, replacements)
+    plot <- df %>%
+      ggplot(aes(x = type, y = value)) +
+      geom_boxplot(staplewidth = 0.1, lwd = 1) +
+      geom_dotplot(
+        aes(fill = gene),
+        binaxis = "y", stackdir = "center", dotsize = 0.5,
+        alpha = 0.8, col = NA
+      ) +
+      ylab("Similarity score") +
+      theme_classic() +
+      theme(
+        title = element_text(size = 16),
+        axis.title.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.line.x = element_blank(),
+        panel.grid = element_blank(),
+        axis.text = element_text(size = 14),
+        plot.background = element_rect(fill = "white")
+      )
+    ggsave(
+      paste0("output/structures/", type, "similarity_scores_boxplot.png"),
+      plot = plot,
+      width = 10, height = 10, dpi = 300
+    )
+  }
 }
-
-df <- data.frame(
-  gene = genes,
-  VS = invivo_vs_insilico,
-  VC = invivo_vs_consensus
-)
-
-write.table(
-  df, "output/similarity_scores.tsv",
-  sep = "\t",
-  row.names = FALSE, quote = FALSE
-)
-
-df <- read.table("output/similarity_scores.tsv", header = TRUE, sep = "\t")
-df <- melt(df)
-colnames(df) <- c("gene", "type", "value")
-df <- df %>%
-  mutate(type = ifelse(type == "VS", "in silico", "consensus"))
-plot <- df %>%
-  ggplot(aes(x = type, y = value)) +
-  geom_boxplot(staplewidth = 0.1, lwd = 1) +
-  geom_dotplot(
-    aes(fill = gene),
-    binaxis = "y", stackdir = "center", dotsize = 0.5,
-    alpha = 0.8, col = NA
-  ) +
-  ylab("Similarity score") +
-  theme_classic() +
-  theme(
-    title = element_text(size = 16),
-    axis.title.x = element_blank(),
-    axis.ticks.x = element_blank(),
-    axis.line.x = element_blank(),
-    panel.grid = element_blank(),
-    axis.text = element_text(size = 14),
-    plot.background = element_rect(fill = "white")
-  )
-ggsave(
-  "output/similarity_scores_boxplot.png",
-  plot = plot,
-  width = 10, height = 10, dpi = 300
-)
--- scripts/generate_structures.sh
+++ scripts/generate_structures.sh
@@ -1,52 +1,55 @@
 BLAST_DB="refseq_select_rna"
 
 root=$(pwd)
-for type in mRNA tRNA rRNA; do
+
+for type in mRNA rRNA tRNA; do
     for gene in $(ls output/structures/$type/); do
-        echo "processing [ $gene ]"
-        cd output/$gene
+        echo "Processing [ $gene ]"
+        cd output/structures/$type/$gene
         mkdir -p \
             vienna/invivo \
             vienna/insilico \
-            vienna/consensus_nn \
-            vienna/consensus_aa \
+            vienna/consensus \
+            vienna/consensus_guided \
             vienna/CT \
             mfold/invivo \
             mfold/insilico \
-            mfold/consensus_nn \
-            mfold/consensus_aa \
+            mfold/consensus \
+            mfold/consensus_guided \
             mfold/CT
-
-        # if [ ! -s seqdump.txt ]; then
-        #     echo "No homologs found! Skipping..."
-        #     cd $root
-        #     continue
-        # fi
-
-        echo "Aligning homologs..."
-        cat nucleotide.fasta seqdump.txt > homologs_nn.fasta
-        clustalw \
-            -INFILE="homologs_nn.fasta" \
-            -OUTFILE="homologs_nn.aln"
 
         cd vienna/insilico
         echo "Generating in silico structure..."
-        RNAfold --noLP -p -d2 -i ../seq.fasta > MFEs.txt
+        RNAfold --noLP -p -d2 -i ../../nucleotide.fasta > MFEs.txt
         b2ct < MFEs.txt > ../CT/insilico.ct
         gs -dNOPAUSE -dBATCH -sDEVICE=png16m -r300 -sOutputFile=ss.png *_ss.ps > /dev/null
         gs -dNOPAUSE -dBATCH -sDEVICE=png16m -r300 -sOutputFile=dp.png *_dp.ps > /dev/null
 
         cd ../invivo
         echo "Generating in vivo structure..."
-        RNAfold --noLP -p -d2 -C -i ../constrained.fasta > MFEs.txt
+        RNAfold --noLP -p -d2 -C -i ../../constrained.fasta > MFEs.txt
         b2ct < MFEs.txt > ../CT/invivo.ct
         gs -dNOPAUSE -dBATCH -sDEVICE=png16m -r300 -sOutputFile=ss.png *_ss.ps > /dev/null
         gs -dNOPAUSE -dBATCH -sDEVICE=png16m -r300 -sOutputFile=dp.png *_dp.ps > /dev/null
 
-        cd ../consensus_nn
+        cd ../..
+
+        if [ ! -s seqdump.txt ]; then
+            echo "No homologs found! Skipping..."
+            cd $root
+            continue
+        fi
+
+        echo "Aligning homologs..."
+        cat nucleotide.fasta seqdump.txt > homologs_nn.fasta
+        clustalw \
+            -INFILE="homologs_nn.fasta" \
+            -OUTFILE="nn.aln"
+
+        cd vienna/consensus
         echo "Generating consensus structure..."
-        RNAalifold --noLP -p -d2 < ../homologs_nn.aln > MFEs.txt
-        b2ct < MFEs.txt > ../CT/consensus_nn.ct
+        RNAalifold --noLP -p -d2 < ../../nn.aln > MFEs.txt
+        b2ct < MFEs.txt > ../CT/consensus.ct
         gs -dNOPAUSE -dBATCH -sDEVICE=png16m -r300 -sOutputFile=alidot.png alidot.ps > /dev/null
         gs -dNOPAUSE -dBATCH -sDEVICE=png16m -r300 -sOutputFile=alirna.png alirna.ps > /dev/null
         cd $root
@@ -54,21 +57,34 @@ for type in mRNA tRNA rRNA; do
 done
 
 for gene in $(ls output/structures/mRNA/); do
-    echo "processing [ $gene ]"
-    cd output/$gene
+    echo "Processing [ $gene ]"
+    cd output/structures/mRNA/$gene
+
+    if [ ! -s hits.csv ]; then
+        echo "No homologs found! Skipping..."
+        cd $root
+        continue
+    fi
+
+    echo "Fetching sequences..."
+    python $root/scripts/hits2fasta.py
 
     echo "Aligning homologs..."
-    cat nucleotide.fasta seqdump.txt > homologs_nn.fasta
+    cat protein.fasta seqdump_aa.txt > homologs_aa.fasta
+    cat nucleotide.fasta pal2nal_guides.txt > pal2nal_guides.fasta
+    rm -f pal2nal_guides.txt
     clustalw \
-        -INFILE="homologs_nn.fasta" \
-        -OUTFILE="homologs_nn.aln"
+        -INFILE="homologs_aa.fasta" \
+        -OUTFILE="homologs_aa.aln"
+    pal2nal.pl homologs_aa.aln pal2nal_guides.fasta > guided.aln
 
-    cd ../consensus_nn
+    cd vienna/consensus_guided
     echo "Generating consensus structure..."
-    RNAalifold --noLP -p -d2 < ../homologs_nn.aln > MFEs.txt
-    b2ct < MFEs.txt > ../CT/consensus_nn.ct
+    RNAalifold --noLP -p -d2 < ../../guided.aln > MFEs.txt
+    b2ct < MFEs.txt > ../CT/consensus_guided.ct
     gs -dNOPAUSE -dBATCH -sDEVICE=png16m -r300 -sOutputFile=alidot.png alidot.ps > /dev/null
     gs -dNOPAUSE -dBATCH -sDEVICE=png16m -r300 -sOutputFile=alirna.png alirna.ps > /dev/null
     cd $root
-    done
+done
+
 exit 0
